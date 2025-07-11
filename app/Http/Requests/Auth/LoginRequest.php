c99e924d10899ed102d08ba88b4e80e9ad173340
chialing55 <clinglu@gmail.com>
2025-07-11 10:57:48 +0800

    Update password confirmation and login request error messages to provide localized feedback for users.

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -68,10 +68,7 @@ class LoginRequest extends FormRequest
         $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
-                'seconds' => $seconds,
-                'minutes' => ceil($seconds / 60),
-            ]),
+            'email' => '登入次數過多，請在 ' . $seconds . ' 秒後再試一次。',
         ]);
     }
 

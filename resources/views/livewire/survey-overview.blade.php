138a40cb5e85da2c93e6d3913762e40ac408e092
chialing55 <clinglu@gmail.com>
2025-10-16 15:56:01 +0800

    feat: Update plot completion display to include entry count and adjust layout

--- resources/views/livewire/survey-overview.blade.php
+++ resources/views/livewire/survey-overview.blade.php
@@ -65,8 +65,8 @@
                     {{-- 完成數 + 達標狀態（手機隱藏 emoji） --}}
                     <div class="w-[110px] md:w-[220px] text-sm">
                         <span
-                            class="{{ $reached ? 'text-green-700 font-semibold' : 'text-red-600' }} w-[90px] inline-block">
-                            {{ $plotDone }} / {{ $plotTotal }} ({{ $plotDonePercent }}%)
+                            class="{{ $reached ? 'text-green-700 font-semibold' : 'text-red-600' }} w-[110px] inline-block">
+                            {{ $plotDone }}<span class='text-gray-500'>+{{$plotEntry}}</span> / {{ $plotTotal }} ({{ $plotDonePercent }}%)
                         </span>
                         <span class="hidden md:inline">
                             @if ($plotDone >= $plotTotal)
